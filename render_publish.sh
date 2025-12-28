68034c995e991884834dcff284d7f267fd70e6f7
muthuspark <muthukrishnan.t@hotmail.com>
2025-12-28 08:31:55 +0530

    Release 2025-12-28 08-31-55

--- render_publish.sh
+++ render_publish.sh
@@ -7,7 +7,7 @@ npx -y pagefind --site public
 echo "Copying ads.txt..."
 cp ads.txt public/ads.txt
 git add .
-current_date=$(date +"%Y-%m-%d %h-%m-%s")
+current_date=$(date +"%Y-%m-%d %H-%M-%S")
 commit_message=${1:-"Release $current_date"}
 git commit -m "$commit_message"
 git push
